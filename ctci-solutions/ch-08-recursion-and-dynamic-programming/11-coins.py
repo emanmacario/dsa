8edaba79a053403d0b848946d72b083a3530a16b
emanmacario <emacario1997@hotmail.com>
2020-08-18 23:48:55 +1000

    Add coin change DP (double counting) question solution

--- ctci-solutions/ch-08-recursion-and-dynamic-programming/11-coins.py
+++ ctci-solutions/ch-08-recursion-and-dynamic-programming/11-coins.py
@@ -6,11 +6,30 @@
 
 # -- Solution
 
-def coins(n):
-    pass
+def make_change(n, coins=[1, 5, 10, 25]):
+    """
+    Uses DP to compute the total number of unique ways of representing
+    n cents, using an infinite number of a set of coins
+
+    Solution is O(n) space and O(nk) time (where k is number of coins).
+    In the case using the standard American coins, the solution is O(n).
+
+    Space complexity has been optimised from O(nk) to O(n) by recognising
+    that after filling the 'j'th row we do not need the 'j-1'th row. 
+    Hence, we continuously overwrite one row during the loop
+    """
+    k = len(coins)
+
+    dp = [1] + [0] * n
+    for i in range(1, k + 1):
+        coin = coins[i-1]
+        for j in range(1, n + 1):
+            dp[j] = dp[j] + (dp[j-coin] if j - coin >= 0 else 0)
+
+    return dp[n]
 
     
 # -- Testing
 
 if __name__ == '__main__':
-    print(coins(12))
+    print(make_change(5, coins=[1, 2, 5]))
